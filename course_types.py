2aad9b218b88c03310166723b873cc69b7c91bfd
lichengl3 <lichengl3@nycstudents.net>
2026-04-28 17:57:55 +0000

    actually fix course_types

--- course_types.py
+++ course_types.py
@@ -15,17 +15,19 @@ def validate_string(string: str) -> str:
 
     return string.replace("'", "''")
 
-
-course_types = []
+course_types = set()
+course_types_class = []
 file_path = "./CourseName,Type,Department.csv"
 
 with open(file_path, 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        course_types.append(CourseTypes(validate_string(row[1])))
-
-course_types = list(dict.fromkeys(course_types)) # remove duplicates
+        validated_string = validate_string(row[1])
+        
+        if not validated_string in course_types:
+            course_types_class.append(CourseTypes(validated_string))
+            course_types.add(validated_string)
 
 with open("./cmd.sql", 'a') as file:
-    for course in course_types:
+    for course in course_types_class:
         print(course, file = file)
