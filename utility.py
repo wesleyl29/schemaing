43a7dd93ada05015c885466dc71993f53e8a2627
lichengl3 <lichengl3@nycstudents.net>
2026-05-01 16:53:52 +0000

    actual input validation

--- utility.py
+++ utility.py
@@ -11,9 +11,9 @@ class Utility:
 
     @staticmethod
     def build_data_csv(file_path: str, column: int, class_name: type) -> list:
-        if not isinstance(column, int) {
+        if not isinstance(column, int):
             raise TypeError("Column should be the index of the desired column of the csv.")
-        }
+        
         
         data = set()
         data_as_class = []
