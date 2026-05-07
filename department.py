cd3224c02314ce83e9f632decdcdcd5a9ec3e024
wesleyl29 <wesleyl29@nycstudents.net>
2026-05-07 17:06:59 +0000

    dept dict

--- department.py
+++ department.py
@@ -1,21 +1,19 @@
 class Department:
-   def __init__(self,department,name):
-       self.department = department
-       self.name = name
+    def __init__(self, department_name: str):
+        self.department_name = department_name
 
-   def __str__(self):
-       return "INSERT INTO Department VALUES("+str(self.department)+", \""+self.name+"\");"
+    def __str__(self):
+        return "INSERT INTO Department (DepartmentName) VALUES (\""+self.department_name+"\");"
 
-   def get_name(self):
-       return self.name
-
-   @staticmethod
-   def get_dict() -> dict:
-       department_dict = {}
-       department_id = 1
-       with open("teacher_data.txt") as file:
-           for line in file:
-               if line.strip() == 'break':
-                   department_dict[department_id] = file.readline().strip()
-                   department_id+=1
-       return department_dict
+    @staticmethod
+    def get_dict() -> dict:
+        dept_dict = {}
+        dept_id = 1
+        with open('teacher_file.txt') as file:
+            for line in file:
+                if line.strip().split(', ', 1)[1] in dept_dict:
+                    pass
+                else:
+                    dept_dict[line.strip().split(', ',1)[1]] = dept_id
+                    dept_id+=1
+        return dept_dict
