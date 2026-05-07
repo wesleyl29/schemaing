4f08e910f4da820e829d2907850ccd4a6f55e2b1
lichengl3 <lichengl3@nycstudents.net>
2026-05-07 18:02:17 +0000

    update class

--- class.py
+++ class.py
@@ -1,6 +1,8 @@
+import random
+
 from course import Course
 from room import Room
-
+from teacher import Teacher
 
 class Class:
     def __init__(self, period: int, room_id: int, teacher_id: int, course_id: int):
@@ -10,12 +12,37 @@ class Class:
         self.course_id = course_id
 
     def __str__(self):
-        return f"INSERT INTO Class ( Period, RoomID, TeacherID, CourseID, ClassID ) VALUES ( {self.period}, {self.room_id}, {self.teacher_id}, {self.course_id} );"
+        return f"INSERT INTO Class ( Period, RoomID, TeacherID, CourseID) VALUES ( {self.period}, {self.room_id}, {self.teacher_id}, {self.course_id} );"
+
+    @classmethod
+    def generate_class(cls) -> list:
+        amount_of_class = []
+        for i in range(10):
+             amount_of_class.append(random.randint(167,200))
+        return amount_of_class
 
     @staticmethod
-    def get_dict():
-        pass
+    def get_dict() -> dict:
+        count = 1
+        class_dict = {}
+        amount = Class.generate_class()
+        
+        for num in amount:
+            for i in range(num):
+                class_dict[f"c{i}p{amount}"] = count
+                count += 1
+        
+        return class_dict
+
+
+def get_random(amount: int, dictionary: dict) -> []:
+    return random.choices(list(dictionary.values()), k=amount)
+
+def get_random_no_duplicates(amount: int, dictionary: dict) -> []:
+    return random.sample(list(dictionary.values()), amount)
+
 
 
 course_dictionary = Course.get_dict()
 room_dictionary = Room.get_dict()
+teacher_dictionary = Teacher.get_dict()
