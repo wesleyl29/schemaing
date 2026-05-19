df1273b2ae69938e0e60ced3bd791917d7caf988
lichengl3 <lichengl3@nycstudents.net>
2026-05-19 16:55:08 +0000

    fix: clean up syntax

--- department.py
+++ department.py
@@ -3,19 +3,19 @@ class Department:
         self.department_name = department_name
 
     def __str__(self):
-        return "INSERT INTO Department (DepartmentName) VALUES (\""+self.department_name+"\");"
+        return f"INSERT INTO Department (DepartmentName) VALUES ('{self.department_name}');"
 
     @staticmethod
     def get_dict() -> dict:
         dept_dict = {}
         dept_id = 1
-        with open('teacher_file.txt') as file:
+        with open("teacher_file.txt", 'r') as file:
             for line in file:
                 if line.strip().split(', ', 1)[1] in dept_dict:
                     pass
                 else:
                     dept_dict[line.strip().split(', ',1)[1]] = dept_id
-                    dept_id+=1
+                    dept_id += 1
         return dept_dict
 
 department_dict = Department.get_dict()
@@ -24,6 +24,6 @@ for key in department_dict:
     dept = Department(key)
     department_list.append(dept)
 
-with open('cmd.sql','a') as f:
+with open("cmd.sql",'a') as f:
     for dept in department_list:
         print(dept, file = f)
