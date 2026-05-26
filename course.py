1783203cbfaca10a095771b5b96577d72059c2f1
lichengl3 <lichengl3@nycstudents.net>
2026-05-26 17:18:22 +0000

    change course

--- course.py
+++ course.py
@@ -3,29 +3,33 @@ from utility import Utility
 
 
 class Course:
-    def __init__(self, course_type_id: int, course: str, course_id: int):
+    course_dict = {}
+
+    def __init__(self, course_type_id: int, course_name: str, course_id: int):
         self.course_type_id = course_type_id
-        self.course = course
+        self.course_name =  course_name
         self.course_id = course_id
+        Course.course_dict[course_name] = course_id
 
     def __str__(self):
-        return f"INSERT INTO Course ( CourseName, CourseTypeID ) VALUES ( '{self.course}', {self.course_type_id} );"
+        return f"INSERT INTO Course ( CourseName, CourseTypeID ) VALUES ( '{self.course_name}', {self.course_type_id} );"
 
     @staticmethod
-    def get_course_dict() -> dict:
-        courses = Utility.build_data_csv("./CourseName,Type,Department.csv", 0)
-        dictionary = {}
-        count = 1
+    def get_id(name: str) -> int:
+        return Course.course_dict[name]
 
-        for course in courses:
-            dictionary[course] = count
-            count += 1
+    @staticmethod
+    def get_course_list() -> list:
+        course_names = Utility.build_data_csv("./CourseName,Type,Department.csv", 0)
+        course_types = Utility.build_data_csv("./CourseName,Type,Department.csv", 1)
+        course_name_type = list(zip(course_names, course_types))
+        course_list = []
 
-        return dictionary
+        for course_id, name_type in enumerate(course_name_type):
+            course_name = name_type[0]
+            course_type = name_type[1]
+            course_type_id = CourseTypes.get_id(course_type)
 
-@staticmethod
-def course_type():
-    course_types_dict = CourseTypes.get_dict()
-    courses = Utility.build_data_csv("./CourseName,Type,Department.csv", 0)
-    course_types = list(course_types_dict)
-    course_with_type = list(zip(courses, course_types))
+            course_list.append(Course(course_type_id, course_name, course_id))
+
+        return course_list
