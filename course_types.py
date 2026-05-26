421113d88c33142408226c868418efea3a522974
lichengl3 <lichengl3@nycstudents.net>
2026-05-26 17:04:21 +0000

    change course_types

--- course_types.py
+++ course_types.py
@@ -2,15 +2,26 @@ from utility import Utility
 
 
 class CourseTypes:
-    def __init__(self, course_type: str):
+    course_type_dict = {}
+
+    def __init__(self, course_type: str, course_type_id: int):
         self.course_type = course_type
+        self.course_type_id = course_type_id
+        CourseTypes.course_type_dict[course_type] = course_type_id
 
     def __str__(self):
         return f"INSERT INTO CourseType ( CourseTypeName ) VALUES ( '{self.course_type}' );"
 
     @staticmethod
-    def coursingtyping():
-        course_types = Utility.build_data_csv_no_duplicates("./CourseName,Type,Department.csv", 1)
-        with open("./cmd.sql", 'a') as file:
-            for course in course_types:
-                print(CourseTypes(course), file=file)
+    def get_id(name: str) -> int:
+        return CourseTypes.course_type_dict[name]
+
+    @staticmethod
+    def get_course_type_list():
+        course_types_name = Utility.build_data_csv_no_duplicates("./CourseName,Type,Department.csv", 1)
+        course_types = []
+
+        for course_type_id, course_type_name in enumerate(course_types_name):
+            course_types.append(CourseTypes(course_type_name, course_type_id))
+
+        return course_types
